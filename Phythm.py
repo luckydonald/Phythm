ad48a0061f1e94160399dadd7a251847d4e9e839
Lucky Donald <admin@luckydonald.de>
2013-10-07 15:01:47 +0200

    Merging Database structure

--- Phythm.py
+++ Phythm.py
@@ -7,118 +7,122 @@ import os
 
 ############################
  ##########################
-  ## 			        ##
-  ## 	 Functions	    ##
-  ##			        ##
+  ##                     ##
+  ##      Functions        ##
+  ##                    ##
  ##########################
 ############################
 
 def processFile(currentDir):
-	''' Process video files within this directory. '''
-	# Get the absolute path of the currentDir parameter
-	currentDir = os.path.abspath(currentDir)
-	# Get a list of files in currentDir
-	filesInCurDir = os.listdir(currentDir)
-	
-	# Traverse through all files
-	for file in filesInCurDir:
-		curFile = os.path.join(currentDir, file)
-		
-		# Check if it's a normal file or directory
-		if os.path.isfile(curFile):
-			curFileExtension = mimetypes.guess_type(curFile)[0]
-			print('+ Guessing file type: %s' % curFileExtension)
-			# Get the file extension
-			
-			# Check if the file has an extension of typical music files
-			if curFileExtension in ['audio/mpeg']:
-				# We have got a audio file! Increment the counter
-				processFile.counter += 1
-				
-				# Print it's name
-				print('+ Found file: %s' % curFile)
-				processFile.list.append(curFile)
-		else:
-			# We got a directory, enter into it for further processing
-			print('# Found dir: %s' % curFile)
-			processFile(curFile)
-				
-			
+    ''' Process video files within this directory. '''
+    # Get the absolute path of the currentDir parameter
+    currentDir = os.path.abspath(currentDir)
+    # Get a list of files in currentDir
+    filesInCurDir = os.listdir(currentDir)
+    
+    # Traverse through all files
+    for file in filesInCurDir:
+        curFile = os.path.join(currentDir, file)
+        
+        # Check if it's a normal file or directory
+        if os.path.isfile(curFile):
+            curFileExtension = mimetypes.guess_type(curFile)[0]
+            print('+ Guessing file type: %s' % curFileExtension)
+            # Get the file extension
+            
+            # Check if the file has an extension of typical music files
+            if curFileExtension in ['audio/mpeg']:
+                # We have got a audio file! Increment the counter
+                processFile.counter += 1
+                
+                # Print it's name
+                print('+ Found file: %s' % curFile)
+                processFile.list.append(curFile)
+        else:
+            # We got a directory, enter into it for further processing
+            print('# Found dir: %s' % curFile)
+            processFile(curFile)
+                
+            
 
-				
-				
+                
+                
 ############################
  ##########################
-  ## 			        ##
-  ## 	    Main	    ##
-  ##			        ##
+  ##                     ##
+  ##         Main        ##
+  ##                    ##
  ##########################
 ############################
 processFile.counter = -1
 
 if __name__ == '__main__':
-	# Get the current working directory
-	currentDir = os.getcwd()
-	currentDir = songdir = settings.conf["music_path"]
+    # Get the current working directory
+    currentDir = os.getcwd()
+    currentDir = songdir = settings.conf["music_path"]
  #"/music"
-	
-	print('Starting processing in %s' % currentDir)
-	
-	# Set the number of processed files equal to zero
-	processFile.counter = 0
-	processFile.list = []
-	
-	# Start Processing
-	processFile(currentDir)
-	
-	# We are done. Continue now. Write Report.
-	print(' -- %s Song File(s) found in directory %s/ --' \
-	  % (processFile.counter, currentDir))
-	print(' Press ENTER to continue!')
-	
-	# Wait until the user presses enter/return
-	raw_input()
-	
-	
-	print('=== Creating Database === ')
-	
-	
-	sqlconn = sqlite3.connect('songs.db')
-	
-	#lets get a cursor!
-	c = sqlconn.cursor()
-	
-	print(' |-> Droping old Database')
-	
-	c.execute('''DROP TABLE IF EXISTS bpm''')
-	print(' |-> Creating Database')
+    
+    print('Starting processing in %s' % currentDir)
+    
+    # Set the number of processed files equal to zero
+    processFile.counter = 0
+    processFile.list = []
+    
+    # Start Processing
+    processFile(currentDir)
+    
+    # We are done. Continue now. Write Report.
+    print(' -- %s Song File(s) found in directory %s/ --' \
+      % (processFile.counter, currentDir))
+    print(' Press ENTER to continue!')
+    
+    # Wait until the user presses enter/return
+    raw_input()
+    
+    
+    print('=== Creating Database === ')
+    
+    
+    sqlconn = sqlite3.connect('music.db')
+    
+    #lets get a cursor!
+    c = sqlconn.cursor()
+    
+    print(' |-> Droping old Database')
+    
+    c.execute('''DROP TABLE IF EXISTS music''')
+    print(' |-> Creating Database')
 
-	#create table
-	#			
-	#		ID   |  BPM  |  PATH            
-	#               0    |  199  |  /music/song.mp3 
-	#
-	print(' |-> Creating Table')
+    #create table
+    #            
+    #        ID     |  BPM  |  PATH            
+    #        0      |  199  |  /music/song.mp3 
+    #
+    print(' |-> Creating Table')
 
-	
-	c.execute('''CREATE TABLE bpm (id  INTEGER PRIMARY KEY, bpm int, path text)''')
-	
-	for file in processFile.list:
-		print(' |')
-		print(' |--> Processing file %s' % file)
-		print(' ||-> Getting Data')
-		audiofile = eyed3.load(file)
-		bpm = audiofile.tag.bpm
-		print(' ||-> BPM: %s' % bpm)
-		if bpm == None:
-			bpm = -1
-			print(' ||#> changing BPM to : %s' % bpm)
-		print(' ||-> Inserting into Database')
-		print(' ||-> (INSERT INTO bpm VALUES (NULL,%s,"%s")' % (bpm, file))
-		c.execute("INSERT INTO bpm VALUES (NULL,%s,\"%s\")" % (bpm, file))
-		print(' |\-> Done.')
-	sqlconn.commit()
-	print(' |-> Commited Database')
-	sqlconn.close()
-	print(' \-> Closed Database')	
+    
+    c.execute('''CREATE TABLE music (id  INTEGER PRIMARY KEY, bpm int, path text)''')
+    
+    for file in processFile.list:
+        print(' |')
+        print(' |--> Processing file %s' % file)
+        print(' ||-> Getting Data')
+        audiofile = eyed3.load(file)
+        bpm = audiofile.tag.bpm
+        print(' ||-> BPM: %s' % bpm)
+        if bpm == None:
+            bpm = -1
+            print(' ||#> changing BPM to : %s' % bpm)
+        print(' ||-> Inserting into Database')
+        print(' ||-> (INSERT INTO music VALUES (NULL,%s,"%s")' % (bpm, unicode(file)))
+        c.execute("INSERT INTO music VALUES (NULL,?,?)" , (bpm, unicode(file)))
+        print(' |\-> Done.')
+    sqlconn.commit()
+    print(' |-> Commited Database')
+    print(' |-> Checking Database')
 
+    for row in c.execute('SELECT * FROM music ORDER BY bpm'):
+        print(row)
+    print(' |-> Closing Database')
+    sqlconn.close()
+    print(' \-> Closed Database')    
